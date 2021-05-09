d3cff91c200091f8c775179aeedb8e4d683be21c
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-05-09 17:22:48 +0300

    style

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 Hello
 
+How 2 Use
 
-How2Run
-
-1 - Add a png that named 'background' to the project folder  <- Very Important\
-2 - Requirements.txt\
+1 - Clone the project\
+2 - Install Dependencies\
 3 - Run the main.py\
-4 - leftclick leftclick leftclick leftclick\
-5 - leftclick again\
-6 - Press Enter when you done placing nodes\
-7 - Ez
+4 - Place nodes with left mouse button\
+5 - Press Enter when you done placing nodes\
 
-dont forget that you MUST add background.png to run main.py  (see ui.py #L14 and you will understand what i mean)
+
+Some Info\
+I perfectly divided output-calculations-userinterface work from each other\
+For example you wanna use different interface method\
+I specified and gave example about input calculations.py needs\
--- main.py
+++ main.py
@@ -4,46 +4,25 @@ from output import Output
 import time
 import os
 
-'''
-Hello
-
-How2Run
-
-1 - Add a png that named 'background' to the project folder  <- Very Important
-2 - Requirements.txt
-3 - Run the main.py
-4 - leftclick leftclick leftclick leftclick
-5 - leftclick again
-6 - Press Enter when you done placing nodes
-7 - Ez
-
-dont forget that you MUST add background.png to run main.py  (see ui.py #L14 and you will understand what i mean)
-'''
-
-# Stuff that you might wanna can change
-node_colour = (0, 0, 0)  # rgb
-line_colour = (255, 255, 255)
-node_thickness = 5
-line_thickness = 5
-
 class Main():
-    def __init__(self, node_colour, line_colour, node_thickness, line_thickness):
-        self.node_colour = node_colour
-        self.line_colour = line_colour
-        self.node_thickness = node_thickness
-        self.line_thickness = line_thickness
+    def __init__(self):
+        # Stuff that you might change
+        self.node_colour = (0, 0, 0)
+        self.line_colour = (255, 255, 255)
+        self.node_thickness = 5
+        self.line_thickness = 5
 
         self.path = None           # Path of minimum distance
         self.distance = None       # Minimum distance, Answer
         self.solve_time = None     # Placing nodes or calculating distance_matrix not included to this
         self.node_locations = None # Node location list ex input = [(x, y), (x, y)]          calculations.py #L39
 
-    def ui_stuff(self): # ui.py
+    def make_user_interface_work(self): # ui.py
         ui_obj = Ui(self.node_colour, self.line_colour, self.node_thickness, self.line_thickness) # creating ui object to get input (node x, y locations)
         self.node_locations = ui_obj.run() # stopping ui object when node_locations are ready
         ui_obj.exit() # closing pygmae window
 
-    def calc_stuff(self): # calculations.py
+    def make_calculations(self): # calculations.py
         os.system('cls')
         print('Solving...')
 
@@ -56,7 +35,7 @@ class Main():
 
         os.system('cls')
 
-    def output_stuff(self): # output.py
+    def give_output(self): # output.py
         output_obj = Output(self.node_locations, self.path, self.distance, self.solve_time, self.node_colour, self.line_colour, self.node_thickness, self.line_thickness) # i have everything i need now i can move onto output.py
         output_obj.print_info()
         output_obj.draw_lines() # first drawing lines looks way cooler lol
@@ -64,8 +43,8 @@ class Main():
         output_obj.save_image()
         output_obj.show_image()
 
-main_obj = Main(node_colour, line_colour, node_thickness, line_thickness)
+main_obj = Main()
 
-main_obj.ui_stuff()
-main_obj.calc_stuff()
-main_obj.output_stuff()
+main_obj.make_user_interface_work() # ui.pu
+main_obj.make_calculations()        # calculations.py
+main_obj.give_output()              # output.py
